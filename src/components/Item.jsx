4ed45f82abb9904dcb6ec7aaf2b11d43e93e7066
Torycd <vicidundun@gmail.com>
2025-06-07 12:17:35 +0100

    challenge completed

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -6,9 +6,11 @@ const Item = ({ num, title, text }) => {
     setIsOpen(!isOpen);
   }
   return (
-    <div className="item" onClick={handleToggle}>
-      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
-      <p className="title">{title}</p>
+    <div className={isOpen ? "open item" : "item"} onClick={handleToggle}>
+      <p className={isOpen ? "open number" : "number"}>
+        {num < 9 ? `0${num + 1}` : num + 1}
+      </p>
+      <p className={isOpen ? "open title" : "title"}>{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
       {isOpen ? <p className="content-box">{text}</p> : ""}
     </div>
